b18036b542b103b6ebfb0e7a0509fbb8445b35dd
Amiya <amiyaranjann5@gmail.com>
2023-07-29 13:25:12 +0530

    adding new files

---
+++ conditional.py
@@ -0,0 +1,25 @@
+a=43
+
+# if elif else ladder in python
+if(a>4):
+    print("the value of a is greater than 4")
+elif(a>5):
+    print("the value of a is greater than 5")
+
+
+elif(a<6):
+    print("the value of a is greater than 5")
+else:
+    print("the value is not greater than4 or 5")
+
+# 2. multiple if statement
+if(a>4):
+    print("the value of a is greater than 4")
+if(a>5):
+    print("the value of a is greater than 5")
+
+
+if(a<6):
+    print("the value of a is lesser than 6")
+else:
+    print("the value is not greater than 4 or 5")
